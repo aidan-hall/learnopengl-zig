77dc9b3d604b0f095ff2dee3c6f86c1ad5b6ac0d
Aidan Hall <aidan.hall@outlook.com>
2021-06-02 18:50:58 +0100

    Rectangle without EBO.

--- src/main.zig
+++ src/main.zig
@@ -4,10 +4,12 @@ const c = @cImport({
 
 const std = @import("std");
 
-const vertexShaderSource = @embedFile("vertex.glsl");
-const fragmentShaderSource = @embedFile("fragment.glsl");
+const ShaderSource = struct {
+    code: []const u8,
+    shaderType: c.GLenum,
+};
 
-const shaderError = error{
+const ShaderError = error{
     CreateShader,
     Compile,
     CreateProgram,
@@ -17,35 +19,58 @@ const shaderError = error{
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
 
+    // zig fmt: off
     var vertices = [_]f32{
-        -0.5, -0.5, 0.0,
-        0.5,  -0.5, 0.0,
-        0.0,  0.5,  0.0,
+        // first triangle
+        0.5, 0.5, 0.0, // top right
+        0.5, -0.5, 0.0, // bottom right
+        -0.5, 0.5, 0.0, // top left
+        // second triangle
+        0.5, -0.5, 0.0, // bottom right
+        -0.5, -0.5, 0.0, // bottom left
+        -0.5, 0.5, 0.0, // top left
     };
+    // zig fmt: on
 
     // Setup.
     var win = c.setup(200, 200, "Nice GLFW", null, null) orelse return error.SiglInit;
     defer c.cleanup(win);
 
-    // shader program
-    var shaderProgram: c.GLuint = try makeShaderProgram(vertexShaderSource, fragmentShaderSource);
+    var shaderProgram = shadProcBlk: {
+        var shaderSources = [_]ShaderSource{
+            ShaderSource{
+                .shaderType = c.GL_VERTEX_SHADER,
+                .code = @embedFile("vertex.glsl"),
+            },
+            ShaderSource{
+                .shaderType = c.GL_FRAGMENT_SHADER,
+                .code = @embedFile("fragment.glsl"),
+            },
+        };
+        var shaders: [shaderSources.len]c.GLuint = undefined;
+        try initShaderStrings(&shaderSources, &shaders);
+        break :shadProcBlk try makeShaderProgram(&shaders);
+    };
     c.glUseProgram(shaderProgram);
 
-    // vertex array object
-    var vao: c_uint = undefined;
+    // vertex array object: Stores attributes for a vbo.
+    var vao: c.GLuint = undefined;
     c.glGenVertexArrays(1, &vao);
     c.glGenBuffers(1, &vao);
     c.glBindVertexArray(vao);
     // vertex buffer object
-    var vbo: c_uint = undefined;
+    var vbo: c.GLuint = undefined;
     c.glGenBuffers(1, &vbo);
     c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
     c.glBufferData(c.GL_ARRAY_BUFFER, @sizeOf(@TypeOf(vertices)), &vertices, c.GL_STATIC_DRAW);
 
     // vertex attributes
-    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), null);
+    c.glVertexAttribPointer(0, 3, try glTypeID(f32), glBool(false), 0, null);
     c.glEnableVertexAttribArray(0);
 
+    //...
+
+    // preparation
     c.glClearColor(0.2, 0.3, 0.3, 1.0);
     c.glBindVertexArray(vao);
 
@@ -57,7 +82,7 @@ pub fn main() anyerror!void {
 
         // Rendering
         c.glClear(c.GL_COLOR_BUFFER_BIT);
-        c.glDrawArrays(c.GL_TRIANGLES, 0, 3);
+        c.glDrawArrays(c.GL_TRIANGLES, 0, 6);
 
         // Events and Buffers
         c.glfwSwapBuffers(win);
@@ -71,49 +96,60 @@ fn processInput(window: *c.GLFWwindow) void {
     }
 }
 
-fn initShader(source: []const u8, shaderType: c.GLenum) shaderError!c.GLuint {
-    // init
-    var shader: c.GLuint = c.glCreateShader(shaderType);
+inline fn initShaderString(source: ShaderSource) ShaderError!c.GLuint {
+    var shader = try createShader(source.shaderType);
 
-    if (shader == 0) {
-        return shaderError.CreateShader;
-    }
+    c.glShaderSource(shader, 1, @ptrCast([*c]const [*c]const u8, &source.code), null);
 
-    {
-        // compile
-        c.glShaderSource(shader, 1, @ptrCast([*c]const [*c]const u8, &source), null);
-        c.glCompileShader(shader);
-
-        var success: c.GLuint = undefined;
-        c.glGetShaderiv(shader, c.GL_COMPILE_STATUS, @ptrCast([*c]c.GLint, &success));
-
-        // log failure
-        if (success != c.GL_TRUE) {
-            var infoLog: [512]u8 = undefined;
-            c.glGetShaderInfoLog(shader, infoLog.len, null, @ptrCast([*c]u8, &infoLog));
-            std.log.err("Shader compilation failed: {}", .{infoLog});
-            return shaderError.Compile;
-        }
-    }
+    try compileShader(shader);
 
     return shader;
 }
 
-fn makeShaderProgram(vertexSource: []const u8, fragmentSource: []const u8) shaderError!c.GLuint {
-    // shaders
-    var vertexShader: c.GLuint = try initShader(vertexSource, c.GL_VERTEX_SHADER);
-    var fragmentShader: c.GLuint = try initShader(fragmentSource, c.GL_FRAGMENT_SHADER);
+fn initShaderStrings(sources: []const ShaderSource, outShaders: []c.GLuint) ShaderError!void {
+    std.debug.assert(sources.len == outShaders.len);
+    for (sources) |source, idx| {
+        outShaders[idx] = try initShaderString(source);
+    }
+}
 
+inline fn createShader(shaderType: c.GLenum) ShaderError!c.GLuint {
+    // init
+    var shader: c.GLuint = c.glCreateShader(shaderType);
+
+    if (shader == 0) {
+        return ShaderError.CreateShader;
+    }
+    return shader;
+}
+
+fn compileShader(shader: c.GLuint) ShaderError!void {
+    c.glCompileShader(shader);
+
+    var success: c.GLuint = undefined;
+    c.glGetShaderiv(shader, c.GL_COMPILE_STATUS, @ptrCast([*c]c.GLint, &success));
+
+    // log failure
+    if (success != c.GL_TRUE) {
+        var infoLog: [512]u8 = undefined;
+        c.glGetShaderInfoLog(shader, infoLog.len, null, @ptrCast([*c]u8, &infoLog));
+        std.log.err("Shader compilation failed: {}", .{infoLog});
+        return ShaderError.Compile;
+    }
+}
+
+fn makeShaderProgram(shaders: []c.GLuint) ShaderError!c.GLuint {
+    // shaders
     var shaderProgram: c.GLuint = c.glCreateProgram();
     if (shaderProgram == 0) {
-        return shaderError.CreateProgram;
+        return ShaderError.CreateProgram;
     }
 
-    c.glAttachShader(shaderProgram, vertexShader);
-    c.glAttachShader(shaderProgram, fragmentShader);
+    for (shaders) |shader| {
+        c.glAttachShader(shaderProgram, shader);
+        c.glDeleteShader(shader);
+    }
 
-    c.glDeleteShader(vertexShader);
-    c.glDeleteShader(fragmentShader);
     c.glLinkProgram(shaderProgram);
 
     // check for errors
@@ -123,8 +159,30 @@ fn makeShaderProgram(vertexSource: []const u8, fragmentSource: []const u8) shade
         var infoLog: [512]u8 = undefined;
         c.glGetProgramInfoLog(shaderProgram, infoLog.len, null, @ptrCast([*c]u8, &infoLog));
         std.log.err("Shader program linking failed: {}", .{infoLog});
-        return shaderError.Link;
+        return ShaderError.Link;
     }
 
     return shaderProgram;
+}
+
+inline fn glBool(cond: bool) c.GLchar {
+    if (cond == true)
+        return c.GL_TRUE
+    else
+        return c.GL_FALSE;
+}
+
+fn glTypeID(comptime attrib: type) !c.GLenum {
+    return switch (attrib) {
+        i8 => c.GL_BYTE,
+        u8 => c.GL_UNSIGNED_BYTE,
+        i16 => c.GL_SHORT,
+        u16 => c.GL_UNSIGNED_SHORT,
+        i32 => c.GL_INT,
+        u32 => c.GL_UNSIGNED_INT,
+        f32 => c.GL_FLOAT,
+        f64 => c.GL_DOUBLE,
+        bool => c.GL_BOOL,
+        else => error.NoKnownGLType,
+    };
 }
