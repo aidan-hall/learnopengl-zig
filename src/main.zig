e3e56c0b629d4a3bfa89ae7cea569521fbe74e94
Aidan Hall <aidan.hall@outlook.com>
2021-06-20 18:49:52 +0100

    Perspective projection for moving squares.

--- src/main.zig
+++ src/main.zig
@@ -8,13 +8,37 @@ usingnamespace @import("utilgl.zig");
 usingnamespace @import("shader.zig");
 usingnamespace @import("matmaths.zig");
 
-fn rotateMatrix2D(angle: f32) mat(4) {
+fn rotateMatrixZ(angle: f32) mat(4) {
     const m = std.math;
     // zig fmt: off
     return mat(4) {
         m.cos(angle), -m.sin(angle), 0, 0,
         m.sin(angle), m.cos(angle), 0, 0,
         0, 0, 1, 0,
+        0, 0, 0, 1,
+    };
+    // zig fmt: on
+}
+
+fn rotateMatrixX(angle: f32) mat(4) {
+    const m = std.math;
+    // zig fmt: off
+    return mat(4) {
+        1, 0, 0, 0,
+        0, m.cos(angle), -m.sin(angle), 0,
+        0, m.sin(angle), m.cos(angle), 0,
+        0, 0, 0, 1,
+    };
+    // zig fmt: on
+}
+
+fn rotateMatrixY(angle: f32) mat(4) {
+    const m = std.math;
+    // zig fmt: off
+    return mat(4) {
+        m.cos(angle), 0, m.sin(angle), 0,
+        0, 1, 0, 0,
+        -m.sin(angle), 0, m.cos(angle), 0,
         0, 0, 0, 1,
     };
     // zig fmt: on
@@ -46,6 +70,15 @@ fn simpleOrthographicProjection(centre: vec(3), scale: vec(3)) mat(4) {
         0, 1.0/scale[1], 0, -scaledCentre[1],
         0, 0, -1.0/scale[2], scaledCentre[2],
         0, 0, 0, 1.0,
+    };
+}
+
+fn perspectiveProjection(l: f32, r: f32, b: f32, t: f32, n: f32, f: f32) mat(4) {
+    return .{
+        2*n/(r-l), 0, (r+l) / (r-l), 0,
+        0, 2*n/(t-b), (b+t) / (t-b), 0,
+        0, 0, - (f + n)/ (f - n), -2 * f * n / (f - n),
+        0, 0, -1, 0,
     };
 }
 
@@ -162,29 +195,29 @@ pub fn main() !void {
     var faceOpacity: f32 = 0.5;
 
     // projection
-    var camCentre = vec(3){ 0.0, 0.0, -1.0 };
-    var camScales = vec(3){ 1.0, 1.0, 100.0 };
+    var eyeCoords = vec(3){ 0.0, 0.0, -3.0 };
+    // var camScales = vec(3){ 1.0, 1.0, 100.0 };
     var viewportSize: [4]f32 = undefined;
 
     // Main Loop
     while (c.glfwWindowShouldClose(win) != c.GLFW_TRUE) {
         var time = @floatCast(f32, c.glfwGetTime());
-        c.glGetFloatv(c.GL_VIEWPORT, &viewportSize);
-        camScales[0] = viewportSize[2] / (1000.0);
-        camScales[1] = viewportSize[3] / (1000.0);
+        // c.glGetFloatv(c.GL_VIEWPORT, &viewportSize);
+        // camScales[0] = viewportSize[2] / (1000.0);
+        // camScales[1] = viewportSize[3] / (1000.0);
         // std.log.info("Viewport size: {} {}.", .{ camScales[0], camScales[1] });
 
         if (c.glfwGetKey(win, c.GLFW_KEY_A) == c.GLFW_PRESS) {
-            camCentre[0] -= 0.05;
+            eyeCoords[0] -= 0.05;
         }
         if (c.glfwGetKey(win, c.GLFW_KEY_D) == c.GLFW_PRESS) {
-            camCentre[0] += 0.05;
+            eyeCoords[0] += 0.05;
         }
         if (c.glfwGetKey(win, c.GLFW_KEY_W) == c.GLFW_PRESS) {
-            camCentre[1] += 0.05;
+            eyeCoords[1] += 0.05;
         }
         if (c.glfwGetKey(win, c.GLFW_KEY_S) == c.GLFW_PRESS) {
-            camCentre[1] -= 0.05;
+            eyeCoords[1] -= 0.05;
         }
         if (c.glfwGetKey(win, c.GLFW_KEY_LEFT) == c.GLFW_PRESS) {
             faceOpacity += 0.05;
@@ -200,11 +233,13 @@ pub fn main() !void {
         }
         try shaderProgram.setUniform(f32, "glfwTime", time * wave_speed);
         try shaderProgram.setUniform(f32, "faceOpacity", faceOpacity);
-        try shaderProgram.setUniform(*mat(4), "projection", &simpleOrthographicProjection(camCentre, camScales));
+
+        var projection = matProd(&.{ translateMatrix(eyeCoords), perspectiveProjection(-0.25, 0.25, -0.25, 0.25, 0.1, 10.0) });
+        try shaderProgram.setUniform(*mat(4), "projection", &projection);
 
         // translated in positive z direction so it will always be in front.
-        var transform = matProd(&[_]mat(4){ rotateMatrix2D(-2.0 * time), translateMatrix(.{ std.math.sin(time), std.math.cos(time), 1.0 }), scaleMatrix(0.25 * (1 + std.math.sin(wave_speed * time / 3))) });
-        var otherTransform = matProd(&[_]mat(4){ scaleMatrix(std.math.sin(time)), translateMatrix(.{ -0.5, 0.5, 0.0 }) });
+        var transform = matProd(&[_]mat(4){ rotateMatrixX(-2.0 * time), rotateMatrixZ(time), translateMatrix(.{ std.math.sin(time), std.math.cos(time), 1.0 }) });
+        var otherTransform = matProd(&[_]mat(4){translateMatrix(.{ -0.5, 0.5, std.math.sin(time) })});
 
         // Rendering
         c.glClear(c.GL_COLOR_BUFFER_BIT | c.GL_DEPTH_BUFFER_BIT);
