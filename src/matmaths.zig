7342a4ffd598855eb531e1a01a0ed36b8da535e4
Aidan Hall <aidan.hall@outlook.com>
2021-06-25 15:48:29 +0100

    [matmaths] Continue expressions for looping.

--- src/matmaths.zig
+++ src/matmaths.zig
@@ -23,6 +23,8 @@ pub const Identity = mat(4){
 };
 
 pub inline fn mat4Coord(x: usize, y: usize) usize {
+    // x: column
+    // y: row
     return x + y * 4;
 }
 
@@ -32,20 +34,16 @@ pub inline fn matProd(matrices: []const mat(4)) mat(4) {
 
     while (index > 0) {
         index -= 1;
-        const next = matrices[index];
         product = new_prod_blk: {
-            // next . product: A . B
+            const next = matrices[index];
             var new_product = mat(4){};
 
             var next_col: usize = 0;
-            while (next_col < 4) {
-                defer next_col += 1;
+            while (next_col < 4) : (next_col += 1) {
                 var product_row: usize = 0;
-                while (product_row < 4) {
-                    defer product_row += 1;
+                while (product_row < 4) : (product_row += 1) {
                     var current_item: usize = 0;
-                    while (current_item < 4) {
-                        defer current_item += 1;
+                    while (current_item < 4) : (current_item += 1) {
                         new_product[mat4Coord(next_col, product_row)] += next[mat4Coord(next_col, current_item)] * product[mat4Coord(current_item, product_row)];
                     }
                 }
