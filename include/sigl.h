5a0ce6d70bb80671d565cf06c56e65cbc4bf33ba
Aidan Hall <aidan.hall@outlook.com>
2021-08-18 14:15:55 +0100

    [sigl.h] Reformatted declarations.

--- include/sigl.h
+++ include/sigl.h
@@ -2,12 +2,10 @@
 #include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
-GLFWwindow* setup(
-		int winWidth, int winHeight,
-		const char* title,
-		GLFWmonitor* monitor, GLFWwindow* share
-		);
-void cleanup(GLFWwindow* win);
+GLFWwindow *setup(int winWidth, int winHeight, const char *title,
+		  GLFWmonitor *monitor, GLFWwindow *share);
+void cleanup(GLFWwindow *win);
 
-void framebufferSizeCallback(GLFWwindow* win, int width, int height);
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void framebufferSizeCallback(GLFWwindow *win, int width, int height);
+void keyCallback(GLFWwindow *window, int key, int scancode, int action,
+		 int mods);
